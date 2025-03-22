c00e7d959aa54749143e8c78819b732250e79396
spec <lydiacharif02@gmail.com>
2025-03-22 02:59:21 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
+import styles from './App.module.css'
 function App() {
   return (
-    <div>
-      AI ChatBot
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/ordinateur.png"/>
+        <h2 className={styles.Title}> AI ChatBot</h2>
+      </header>
+      <div/>
+
     </div>
 );
 
