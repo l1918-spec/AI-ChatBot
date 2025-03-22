701aeb04bde904ac58dbef8f7f3a7b22793e24a3
spec <lydiacharif02@gmail.com>
2025-03-22 21:48:24 +0100

    separating

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import styles from './App.module.css'
 import { Chat } from './components/Chat/Chat';
 import { Controls } from './components/Controls/Controls';
-import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const googleai = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_AI_API_KEY);
-const gemini = googleai.getGenerativeModel({ model: "gemini-1.5-flash" })
-const chat = gemini.startChat({history: []});
+import { Assistant } from './assistants/googleai';
+
+
 function App() {
+  const assistant = new Assistant();
   const [messages, setMessages] = useState([]); //set messages to store them
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message ]);
@@ -16,8 +16,8 @@ function App() {
   async function handleContentSend(content) {
     addMessage({content, role: 'user'});
     try {
-      const result = await chat.sendMessage(content) //async
-      addMessage({content: result.response.text(), role: 'assistant'});
+      const result = await assistant.chat(content);
+      addMessage({content: result, role: 'assistant'});
     }
     catch (error) {
       addMessage({content:"Sorry, I Couldnt Help", role: 'system'});
