5bf00cf4f5d9bdcc68de7c3a45b7aa4225f41eeb
spec <lydiacharif02@gmail.com>
2025-03-23 04:04:21 +0100

    laoding

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,33 @@ import { Chat } from './components/Chat/Chat';
 import { Controls } from './components/Controls/Controls';
 
 import { Assistant } from './assistants/googleai';
+import { Loader } from './components/Loader/Loader';
 
 
 function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]); //set messages to store them
+  const[isloading, setLoading] = useState(false);
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message ]);
   }
 
   async function handleContentSend(content) {
-    addMessage({content, role: 'user'});
+    addMessage({ content, role: 'user' });
+    setLoading(true);
     try {
       const result = await assistant.chat(content);
       addMessage({content: result, role: 'assistant'});
     }
     catch (error) {
       addMessage({content:"Sorry, I Couldnt Help", role: 'system'});
+    } finally {
+      setLoading(false);
     }
   }
   return (
     <div className={styles.App}>
+      {isloading && <Loader />}
       <header className={styles.Header}>
         <img className={styles.Logo} src="/ordinateur.png"/>
         <h2 className={styles.Title}> AI ChatBot</h2>
