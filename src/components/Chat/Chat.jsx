263f17a5a9f1a84accfe6f462d8d49c55fcdcb1e
spec <lydiacharif02@gmail.com>
2025-03-23 06:32:57 +0100

    dark-light mode

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,28 +1,41 @@
 import styles from './Chat.module.css';
-import React from 'react';
+import React, { useMemo, useEffect, useState, useRef } from 'react';
 import Markdown from 'react-markdown';
-import { useEffect, useState, useRef } from 'react';
 
 const WELCOME_MESSAGE = {
     role: 'assistant',
     content: "Hello! How can I help you today?"
-}
-export function Chat({ messages }) {
-    const messagesEndRef = useRef(null)
-    useEffect(() => {
-        messagesEndRef.current?.scrollIntoView({behavior:'smooth'})
-    }, [messages])
+};
 
+export function Chat({ messages }) {
+    const messagesEndRef = useRef(null);
+
+    const messagesGroups = useMemo(() =>
+        messages.reduce((groups, message) => {
+            const lastGroup = groups[groups.length - 1];
+            if (lastGroup && lastGroup.role === message.role) {
+                lastGroup.messages.push(message);
+            } else {
+                groups.push({
+                    role: message.role,
+                    messages: [message]
+                });
+            }
+            return groups;
+        }, []),
+        [messages] 
+    ); 
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]); 
 
     return (
         <div className={styles.Chat}>
             {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
-            <div>
-                
                 <div key={index} data-role={role} className={styles.Messages}>
                     <Markdown>{content}</Markdown>
-                    </div>
-            </div>
+                </div>
             ))}
             <div ref={messagesEndRef} />
         </div>
